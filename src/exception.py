2f8f7ce60eff48319e7f0cd735710cc58ed0c421
shubham.phalak <phalakshubham@gmail.com>
2025-04-18 15:32:53 +0530

    exception mesg

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
 
-def error_2message_detail(error,error_details:sys):
+def error_message_detail(error,error_details:sys):
     _,_,exc_tb = error_details.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occurred in script name [{0}] line number [{1}] error message [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
